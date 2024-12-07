51e935c3fd15ba76e7eb7931d794b3f732e19c0f
xsbfh <z6MkpzZDoFM7tPZPXqvaVCtMFxKJQAxghVRJZxsf8s8SqBvm@dev.smashchats.com>
2024-12-07 21:24:30 +0000

    upgrade typescript version
    Signed-off-by: xsbfh <z6MkpzZDoFM7tPZPXqvaVCtMFxKJQAxghVRJZxsf8s8SqBvm@dev.smashchats.com>

--- scripts/generate-identity-hsm.ts
+++ scripts/generate-identity-hsm.ts
@@ -1,7 +1,8 @@
 #!/bin/node
-import { IECKeyPair, Identity, setEngine } from '2key-ratchet';
+import { IECKeyPair, setEngine } from '2key-ratchet';
 import 'dotenv/config';
 import { Crypto as CryptoP11 } from 'node-webcrypto-p11';
+import { SmashMessaging } from 'smash-node-lib';
 import { SPLITTER } from 'src/crypto.js';
 
 const sleep = (ms: number) =>
@@ -15,8 +16,8 @@ async function main() {
         return process.exit(1);
     }
     const config = JSON.parse(process.env.HSM_CONFIG);
-    const crypto = new CryptoP11(config);
-    setEngine('@peculiar/webcrypto', crypto as unknown as Crypto);
+    const c = new CryptoP11(config);
+    setEngine('@peculiar/webcrypto', c as unknown as Crypto);
 
     // const NODE_PATH = process.argv[0];
     // const SCRIPT_PATH = process.argv[1];
@@ -27,7 +28,6 @@ async function main() {
         return process.exit(1);
     }
 
-    const DID = parseInt(process.argv[2] || '0');
     const NB_PREKEYS = parseInt(process.argv[3] || '0');
     const NB_ONETIME = parseInt(process.argv[4] || '0');
     const SKIP_CLEAR_STORAGE = !!process.argv[5] || false;
@@ -41,22 +41,22 @@ async function main() {
             );
             await sleep(1100);
         }
-        await crypto.keyStorage.clear();
+        await c.keyStorage.clear();
     }
 
     // generate new Identity, generating as many pre-keys and one-time pre-keys as requested
-    const identity = await Identity.create(DID, NB_PREKEYS, NB_ONETIME, false);
+    SmashMessaging.setCrypto(c);
+    const identity = await SmashMessaging.generateIdentity(
+        NB_PREKEYS,
+        NB_ONETIME,
+    );
 
     // persisting all the newly created keys to HSM
     const persistKeyPairToHSMStorage = async (key: IECKeyPair) => {
         const trim = (id: string) => id.split(SPLITTER)[2];
         const thumb = key.publicKey.id;
-        const privateKeyId = trim(
-            await crypto.keyStorage.setItem(key.privateKey),
-        );
-        const publicKeyId = trim(
-            await crypto.keyStorage.setItem(key.publicKey.key),
-        );
+        const privateKeyId = trim(await c.keyStorage.setItem(key.privateKey));
+        const publicKeyId = trim(await c.keyStorage.setItem(key.publicKey.key));
         if (privateKeyId !== publicKeyId)
             throw new Error("Private and Public Keys HSM IDs don't match.");
         return [thumb, publicKeyId];
